2bf0711d3d9aa4af02bcd5a6de00537cffee02a8
calakdao <calakdao@gmail.com>
2024-11-14 18:31:07 +0700

    .

--- menu.php
+++ menu.php
@@ -238,14 +238,6 @@
                             </a>
                         </li><!--end nav-item-->
                     </ul><!--end navbar-nav--->
-                    <div class="update-msg text-center"> 
-                        <div class="d-flex justify-content-center align-items-center thumb-lg update-icon-box  rounded-circle mx-auto">
-                            <i class="iconoir-peace-hand h3 align-self-center mb-0 text-primary"></i>
-                        </div>                   
-                        <h5 class="mt-3">Mannat Themes</h5>
-                        <p class="mb-3 text-muted">Rizz is a high quality web applications.</p>
-                        <a href="javascript: void(0);" class="btn text-primary shadow-sm rounded-pill">Upgrade your plan</a>
-                    </div>
                 </div>
             </div><!--end startbar-collapse-->
         </div><!--end startbar-menu-->    
